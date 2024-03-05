bf63d63aaf7dec135277a690a65ac22fe46435f6
JorgenMJobloop <jorgen@jobloop.no>
2024-03-05 13:08:57 +0100

    Add files via upload

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+    return (
+        <>
+            <section>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="React-logo" width="200px" height="200px"></img>
+                <p>ReactJS hero component!</p>
+            </section >
+        </>
+    )
+}
